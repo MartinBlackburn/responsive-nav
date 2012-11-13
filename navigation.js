219d8946bf0fc4827a0ea984c202ebf9c6f8d802
Martin Blackburn <walking_fishy@hotmail.com>
2012-11-13 19:20:15 +0000

    added navigation li via js, will use it to toggle open/closing of nav

--- navigation.js
+++ navigation.js
@@ -1,12 +1,49 @@
-ResponsiveNav = function(nav) 
+ResponsiveNav = function(nav, breakPoint) 
 {   
+	//varibles
+	var width = $(document).width();
+
+	//add nav controls
+	var navControl = $("<li class='navControls'/>").prependTo(nav.find("ul").first());
+    var navControlLink = $('<a/>', {
+        "text": "Navigation",
+        click: function() {
+            toggleNav();
+        }
+    }).prependTo(navControl);
+    
+    //check if to hide or show controls
+ 	checkNav();
 	
+	//listener for screen width
+	$(window).resize(function() {
+		width = $(document).width();
+		checkNav();
+	});
+	
+	//hide or show nav controls depending on breakpoint
+    function checkNav()
+    {
+    	if(width > breakPoint)
+    	{
+    		$("navControls").hide();
+    	}
+    	else {
+    		$("navControls").show();
+    	}
+    }
+	
+	//open or close nav
+    function toggleNav()
+    {
+    	alert("nav toggle");
+    }
 };
 
 $(function() 
 {
     $(".nav").each(function()
     {
-        new ResponsiveNav($(this));
+        new ResponsiveNav($(this), 500);
     });
 });
